74866b2c7beb9f02dd785f17f970196dd2c3656b
Eshan Roy (Eshanized) <src.eshan@gmail.com>
2023-12-17 20:44:35 +0530

    👾 deleted sddm -> Later I will make a config

--- cred4github.sh
+++ cred4github.sh
@@ -1,36 +1,13 @@
 #!/bin/bash
-#set -e
-##################################################################################################################
+
 # Author 	: Erik Dubois
 # Website   : https://www.erikdubois.be
-# Website   : https://www.alci.online
-# Website	: https://www.arcolinux.info
-# Website	: https://www.arcolinux.com
-# Website	: https://www.arcolinuxd.com
-# Website	: https://www.arcolinuxb.com
-# Website	: https://www.arcolinuxiso.com
-# Website	: https://www.arcolinuxforum.com
-##################################################################################################################
-#
-#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
-#
-##################################################################################################################
-#tput setaf 0 = black 
-#tput setaf 1 = red 
-#tput setaf 2 = green
-#tput setaf 3 = yellow 
-#tput setaf 4 = dark blue 
-#tput setaf 5 = purple
-#tput setaf 6 = cyan 
-#tput setaf 7 = gray 
-#tput setaf 8 = light blue
-##################################################################################################################
 
 echo
 tput setaf 3
-echo "################################################################"
+echo ""
 echo "################### Start"
-echo "################################################################"
+echo ""
 tput sgr0
 echo
 
@@ -54,22 +31,22 @@ echo
 #https://blog.nillsf.com/index.php/2021/05/27/github-sso-using-password-protected-ssh-keys
 
 project=$(basename `pwd`)
-githubdir="arcolinux"
+githubdir="snigdhalinux"
 echo "-----------------------------------------------------------------------------"
 echo "this is project https://github.com/$githubdir/$project"
 echo "-----------------------------------------------------------------------------"
 
 git config --global pull.rebase false
 git config --global push.default simple
-git config --global user.name "arcolinuxz"
-git config --global user.email "arcolinuxinfo@gmail.com"
+git config --global user.name "eshanized"
+git config --global user.email "src.eshan@gmail.com"
 sudo git config --system core.editor nano
 git remote set-url origin git@github.com-arc:$githubdir/$project
 
 echo
 tput setaf 3
-echo "################################################################"
+echo ""
 echo "################### End"
-echo "################################################################"
+echo ""
 tput sgr0
 echo
